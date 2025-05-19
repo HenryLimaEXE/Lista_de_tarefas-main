f865cfe61760c9e42f528a1644fdba51dd8e866c
HenryExe <henrycarvalho282@gmail.com>
2025-05-19 13:02:02 -0300

    Features - Tela de Login - Tela de Cadastro - LocalStorage -> Armazenamento de dados usuário - remove Header component - Angular Material - Validações (Login & Cadastro)

--- src/app/pages/lista-tarefas/lista-tarefas.component.ts
+++ src/app/pages/lista-tarefas/lista-tarefas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarefa } from '../../shared/models/tarefa.interface';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-tarefas',
@@ -9,6 +10,8 @@ import Swal from 'sweetalert2';
 })
 
 export class ListaTarefasComponent implements OnInit {
+  userName: string | null = null;
+  loggedUser: any = null;
   tarefas: Tarefa[] = [];
   tarefasPendentes: Tarefa[] = [];
   tarefasConcluidas: Tarefa[] = [];
@@ -18,7 +21,7 @@ export class ListaTarefasComponent implements OnInit {
   novaDescricao = '';
   dataInvalida: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     const tarefasFromStorage = localStorage.getItem('tarefas');
@@ -26,6 +29,28 @@ export class ListaTarefasComponent implements OnInit {
       this.tarefas = JSON.parse(tarefasFromStorage);
       this.atualizarListas();
     }
+
+    this.loggedUser = JSON.parse(localStorage.getItem('loggedUser') || '{}');
+
+    if (this.loggedUser && this.loggedUser.id) {
+      this.userName = this.loggedUser.name;
+      this.carregarTarefas();
+    }
+  }
+
+  carregarTarefas() {
+    const userKey = `tarefas_${this.loggedUser.id}`;
+
+    const tarefasFromStorage = localStorage.getItem(userKey);
+    if (tarefasFromStorage) {
+      this.tarefas = JSON.parse(tarefasFromStorage);
+      this.atualizarListas();
+    }
+  }
+
+  salvarTarefas() {
+    const userKey = `tarefas_${this.loggedUser.id}`;
+    localStorage.setItem(userKey, JSON.stringify(this.tarefas));
   }
 
   formatarData(event: any): void {
@@ -53,13 +78,13 @@ export class ListaTarefasComponent implements OnInit {
 
     const [dia, mes, ano] = data.split('/').map(Number);
 
-    // Valida o dia e o mês
     if (dia < 1 || dia > 31 || mes < 1 || mes > 12) {
       this.dataInvalida = true;
     } else {
       this.dataInvalida = false;
     }
   }
+
 
   //#region CRUD TARFAS
 
@@ -74,11 +99,9 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   adicionarTarefa() {
-
     if (this.novaTarefa.trim() == '' || this.novaDataLimite.trim() == '' || this.novaDescricao.trim() == '') {
-
-      Swal.fire("É Necessário preencher todos os campos para adicionar uma nova tafera");
-
+      Swal.fire("É Necessário preencher todos os campos para adicionar uma nova tarefa");
+      return;
     }
 
     if (this.novaTarefa.trim() !== '' && this.novaDataLimite.trim() !== '' && this.novaDescricao.trim() !== '') {
@@ -93,12 +116,11 @@ export class ListaTarefasComponent implements OnInit {
       this.novaDataLimite = '';
       this.novaDescricao = '';
 
-      localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
+      this.salvarTarefas();
     }
   }
 
   async editarTarefa(index: number, lista: { tarefa: string, dataLimite: string, descricao: string, concluida: boolean }[]) {
-    // Editar Tarefa
     const { value: novaTarefa } = await Swal.fire({
       title: 'Editar Tarefa',
       input: 'text',
@@ -142,9 +164,8 @@ export class ListaTarefasComponent implements OnInit {
       lista[index].descricao = novaDescricao;
     }
 
-    // Atualiza o localStorage apenas uma vez no final
     if (novaTarefa || novaDataLimite || novaDescricao) {
-      this.atualizarLocalStorage();
+      this.salvarTarefas();
     }
   }
 
@@ -163,12 +184,12 @@ export class ListaTarefasComponent implements OnInit {
           const taskIndex = this.tarefas.findIndex(task => task === lista[index]);
           if (taskIndex !== -1) {
             this.tarefas.splice(taskIndex, 1);
-            this.atualizarLocalStorage();
+            this.salvarTarefas();
           }
         }
         lista.splice(index, 1);
         this.atualizarListas();
-        this.atualizarLocalStorage();
+        this.salvarTarefas();
       }
     });
   }
@@ -182,7 +203,7 @@ export class ListaTarefasComponent implements OnInit {
   alternarStatusTarefa(index: number, lista: { tarefa: string, dataLimite: string, descricao: string, concluida: boolean }[]) {
     lista[index].concluida = !lista[index].concluida;
     this.atualizarListas();
-    this.atualizarLocalStorage();
+    this.salvarTarefas();
   }
 
   moverParaFazendo(index: number, lista: { tarefa: string, dataLimite: string, descricao: string, concluida: boolean }[]) {
@@ -190,7 +211,7 @@ export class ListaTarefasComponent implements OnInit {
     if (tarefaSelecionada) {
       lista.splice(index, 1);
       this.tarefasAFazer.push(tarefaSelecionada);
-      this.atualizarLocalStorage();
+      this.salvarTarefas();
     }
   }
 
@@ -198,6 +219,70 @@ export class ListaTarefasComponent implements OnInit {
     localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
   }
 
-  //#endregion
+  redirecionarParaRedefinirSenha() {
+    const loggedUser = JSON.parse(localStorage.getItem('loggedUser') || '{}');
+    const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+
+    if (!loggedUser) {
+      Swal.fire('Você não está logado!', 'Faça login para redefinir sua senha.', 'error');
+      return;
+    }
+
+    Swal.fire({
+      title: 'Redefinir Senha',
+      html: `
+      <div class="swal-form">
+        <input id="currentPassword" type="password" class="swal2-input" placeholder="Digite a senha original">
+        <input id="newPassword" type="password" class="swal2-input" placeholder="Digite a nova senha">
+        <input id="confirmNewPassword" type="password" class="swal2-input" placeholder="Confirme a nova senha">
+      </div>
+    `,
+      focusConfirm: false,
+      showCancelButton: true,
+      confirmButtonText: 'Alterar',
+      allowOutsideClick: false,
+      preConfirm: () => {
+        const currentPassword = (document.getElementById('currentPassword') as HTMLInputElement).value;
+        const newPassword = (document.getElementById('newPassword') as HTMLInputElement).value;
+        const confirmNewPassword = (document.getElementById('confirmNewPassword') as HTMLInputElement).value;
+
+        if (!currentPassword || !newPassword || !confirmNewPassword) {
+          Swal.showValidationMessage('Todos os campos são obrigatórios');
+          return false;
+        }
+
+        if (loggedUser.password !== currentPassword) {
+          Swal.showValidationMessage('A senha original está incorreta');
+          return false;
+        }
+
+        if (newPassword !== confirmNewPassword) {
+          Swal.showValidationMessage('As novas senhas não coincidem');
+          return false;
+        }
+
+        loggedUser.password = newPassword;
+        localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+
+        const userIndex = storedUsers.findIndex((user: any) => user.email === loggedUser.email);
+        if (userIndex !== -1) {
+          storedUsers[userIndex].password = newPassword;
+          localStorage.setItem('users', JSON.stringify(storedUsers));
+        }
+
+        return true;
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Senha Atualizada!', 'Sua senha foi atualizada com sucesso.', 'success');
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('loggedUser');
+    this.router.navigate(['/login']);
+  }
+
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarefa } from '../../shared/models/tarefa.interface';
 import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,99 @@ import Swal from 'sweetalert2';
 
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  hidePassword = true;
+  isLoading = false;
+  loading = false;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
+  ngOnInit() { }
+
+  async navigateToRegister(event?: Event) {
+    if (event) {
+      event.preventDefault();
+    }
+
+    Swal.fire({
+      title: 'Carregando...',
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    Swal.close();
+    this.router.navigate(['/cadastrar']);
+  }
+
+
+  VerificarLogin() {
+    const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (storedUsers.length === 0) {
+      this.snackBar.open('Nenhum usuário cadastrado encontrado', 'Fechar', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
+    const foundUser = storedUsers.find((user: any) => user.email === email);
+
+    if (!foundUser) {
+      this.snackBar.open('E-mail não cadastrado', 'Fechar', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
+    if (foundUser.password === password) {
+      this.snackBar.open('Login realizado com sucesso!', 'Fechar', {
+        duration: 3000,
+        panelClass: ['success-snackbar']
+      });
+
+      localStorage.setItem('loggedUser', JSON.stringify({
+        id: foundUser.id,
+        name: foundUser.name_user,
+        email: foundUser.email
+      }));
+
+      Swal.fire({
+        title: 'Carregando...',
+        text: 'Redirecionando para seus agendamentos',
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        }
+      });
+
+      setTimeout(() => {
+        Swal.close();
+        this.router.navigate(['/lista-tarefas']);
+      }, 1500);
+
+    } else {
+      this.snackBar.open('Senha incorreta', 'Fechar', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      });
+    }
+  }
 }
